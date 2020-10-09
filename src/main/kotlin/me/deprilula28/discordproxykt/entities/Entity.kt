d20ffa9d6829fc34329798b5d83a25f6ad873018
deprilula28 <deprilula28@gmail.com>
2020-10-08 22:57:51 -0300

    Entity Managers

--- src/main/kotlin/me/deprilula28/discordproxykt/entities/Entity.kt
+++ src/main/kotlin/me/deprilula28/discordproxykt/entities/Entity.kt
@@ -7,11 +7,13 @@ import kotlinx.serialization.json.JsonPrimitive
 import me.deprilula28.discordproxykt.DiscordProxyKt
 import me.deprilula28.discordproxykt.rest.IRestAction
 import me.deprilula28.discordproxykt.rest.RestAction
-import me.deprilula28.discordproxykt.rest.RestEndpoint
 import java.awt.Color
 import java.time.ZonedDateTime
 import java.util.*
+import java.util.concurrent.CompletableFuture
 import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 /// Interfaces deriving this only need the ID to run the included functions.
 interface IPartialEntity {
@@ -19,18 +21,20 @@ interface IPartialEntity {
     val bot: DiscordProxyKt
 }
 
+interface EntityManager: IRestAction<Unit> {
+    val edits: MutableMap<String, JsonElement>
+}
+
 /// Classes deriving this will be Discord entities, with an ID and delegated fields.
 open class Entity(private val map: JsonObject, override val bot: DiscordProxyKt): IPartialEntity {
-    @Deprecated("JDA Compatibility Field", ReplaceWith("bot"))
-    val jda: DiscordProxyKt by ::bot
+    
+    @Deprecated("JDA Compatibility Field", ReplaceWith("bot")) val jda: DiscordProxyKt by ::bot
     
     override val snowflake: Snowflake by map.delegateJson(JsonElement::asSnowflake, "id")
     
-    @Deprecated("JDA Compatibility Field", ReplaceWith("snowflake.id"))
-    val id: String by snowflake::id
+    @Deprecated("JDA Compatibility Field", ReplaceWith("snowflake.id")) val id: String by snowflake::id
     
-    @Deprecated("JDA Compatibility Field", ReplaceWith("snowflake.idLong"))
-    val idLong: Long by snowflake::idLong
+    @Deprecated("JDA Compatibility Field", ReplaceWith("snowflake.idLong")) val idLong: Long by snowflake::idLong
 }
 
 fun JsonElement.asString() = (this as JsonPrimitive).content
@@ -43,17 +47,52 @@ fun JsonElement.asTimestamp(): Timestamp {
     return Timestamp(Date.from(ZonedDateTime.parse(asString()).toInstant()).time)
 }
 
-inline fun <reified T> JsonObject.delegateJson(crossinline func: JsonElement.() -> T, field: String? = null): ReadOnlyProperty<Any?, T>
-    = ReadOnlyProperty { _, property ->
-        func(this[field ?: property.name]!!)
-    }
+inline fun <reified T> getValue(obj: JsonObject, field: String, crossinline func: JsonElement.() -> T): T = func(
+    obj[field]!!)
 
-inline fun <reified T> JsonObject.delegateJsonNullable(crossinline func: JsonElement.() -> T?, field: String? = null): ReadOnlyProperty<Any?, T?>
-    = ReadOnlyProperty { _, property ->
-        val prop = this[field ?: property.name]
-        if (prop == null || prop == JsonNull || prop.toString() == "null") null
-        else func(prop)
+inline fun <reified T> getValueNullable(obj: JsonObject, field: String, crossinline func: JsonElement.() -> T): T? {
+    val prop = obj[field]
+    return if (prop == null || prop == JsonNull || prop.toString() == "null") null
+    else func(prop)
+}
+
+inline fun <reified T> JsonObject.delegateJson(
+    crossinline func: JsonElement.() -> T,
+    field: String? = null,
+): ReadOnlyProperty<Any?, T> = ReadOnlyProperty { _, property -> getValue(this, field ?: property.name, func) }
+
+inline fun <reified T> JsonObject.delegateJsonNullable(
+    crossinline func: JsonElement.() -> T?,
+    field: String? = null,
+): ReadOnlyProperty<Any?, T?> = ReadOnlyProperty { _, property ->
+    getValueNullable(this@delegateJsonNullable, field ?: property.name, func)
+}
+
+inline fun <reified T> JsonObject.delegateJsonMutable(
+    crossinline read: JsonElement.() -> T,
+    crossinline write: (T) -> JsonElement,
+    field: String? = null,
+): ReadWriteProperty<EntityManager, T> = object: ReadWriteProperty<EntityManager, T> {
+    override fun getValue(thisRef: EntityManager, property: KProperty<*>): T = getValue(this@delegateJsonMutable,
+                                                                                        field ?: property.name, read)
+    
+    override fun setValue(thisRef: EntityManager, property: KProperty<*>, value: T) {
+        thisRef.edits[field ?: property.name] = write(value)
     }
+}
+
+inline fun <reified T> JsonObject.delegateJsonMutableNullable(
+    crossinline read: JsonElement.() -> T?,
+    crossinline write: (T) -> JsonElement,
+    field: String? = null,
+): ReadWriteProperty<EntityManager, T?> = object: ReadWriteProperty<EntityManager, T?> {
+    override fun getValue(thisRef: EntityManager, property: KProperty<*>): T? = getValueNullable(
+        this@delegateJsonMutableNullable, field ?: property.name, read)
+    
+    override fun setValue(thisRef: EntityManager, property: KProperty<*>, value: T?) {
+        thisRef.edits[field ?: property.name] = write(value!!)
+    }
+}
 
 // https://stackoverflow.com/questions/5346477/implementing-a-bitfield-using-java-enums
 inline fun <reified E: Enum<E>> Long.bitSetToEnumSet(values: Array<E>): EnumSet<E> {
