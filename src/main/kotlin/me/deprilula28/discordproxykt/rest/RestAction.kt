97eff220782dd6d81bbf8cf80edc5687b088fe94
deprilula28 <deprilula28@gmail.com>
2020-10-11 01:28:21 -0300

    Add rate limits

--- src/main/kotlin/me/deprilula28/discordproxykt/rest/RestAction.kt
+++ src/main/kotlin/me/deprilula28/discordproxykt/rest/RestAction.kt
@@ -8,8 +8,8 @@ import java.net.URI
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
 
-// TODO Support rate limits
 open class RestAction<T: Any>(
     override val bot: DiscordProxyKt,
     private val constructor: JsonElement.(DiscordProxyKt) -> T,
@@ -19,15 +19,16 @@ open class RestAction<T: Any>(
 ): IRestAction<T> {
     companion object {
         const val DISCORD_PATH: String = "https://discord.com/api/v8/"
+        val routeRateLimits = ConcurrentHashMap<RestEndpoint, RateLimitBucket>()
+        
+        data class RateLimitBucket(
+            var remaining: Int,
+            var resetEpochSecs: Long,
+            var waitingFuture: CompletableFuture<Unit>? = null,
+        )
     }
     
-    /// Send the REST action and return a Java future
-    override fun request(): CompletableFuture<T> {
-        val path = endpoint.path(*pathParts)
-        if (endpoint.method == RestEndpoint.Method.GET) {
-            val item = bot.cache.retrieve<T>(path)
-            if (item != null) return CompletableFuture.completedFuture(item)
-        }
+    private fun createRequest(path: String, bucket: RateLimitBucket): CompletableFuture<T> {
         val request = HttpRequest.newBuilder()
             .uri(URI(DISCORD_PATH + path))
             .method(endpoint.method.toString(), postData?.run { HttpRequest.BodyPublishers.ofString(this()) }
@@ -36,6 +37,13 @@ open class RestAction<T: Any>(
             .header("User-Agent", "DiscordBot (https://github.com/deprilula28/discord-proxy-kt v0.0.1)")
             .build()
         return bot.client.sendAsync(request, HttpResponse.BodyHandlers.ofInputStream()).thenApply {
+            val rateLimitRemaining = it.headers().firstValue("X-RateLimit-Remaining").get().toInt()
+            val rateLimitReset = it.headers().firstValue("X-RateLimit-Reset").get().toLong()
+            synchronized(bucket) {
+                bucket.remaining = rateLimitRemaining
+                bucket.resetEpochSecs = rateLimitReset
+            }
+        
             if (it.statusCode() != 200)
                 throw RestException(
                     request.uri().toString(),
@@ -46,6 +54,33 @@ open class RestAction<T: Any>(
             if (endpoint.method == RestEndpoint.Method.GET) bot.cache.store(path, res)
             constructor(res, bot)
         }
+    }
+    
+    /**
+     * Sends this REST action and returns a Java future.
+     */
+    override fun request(): CompletableFuture<T> {
+        val path = endpoint.path(*pathParts)
+        if (endpoint.method == RestEndpoint.Method.GET) {
+            val item = bot.cache.retrieve<T>(path)
+            if (item != null) return CompletableFuture.completedFuture(item)
+        }
+        val bucket = routeRateLimits[endpoint] ?: RateLimitBucket(1, -1L)
+        if (!routeRateLimits.containsKey(endpoint)) routeRateLimits[endpoint] = bucket
+        
+        synchronized(bucket) {
+            val time = bucket.resetEpochSecs
+            // Join in the waiting future
+            if (bucket.waitingFuture != null) return bucket.waitingFuture!!.thenCompose { createRequest(path, bucket) }
+            if (bucket.remaining == 0) {
+                val wait = CompletableFuture.supplyAsync { Thread.sleep(System.currentTimeMillis() - (time * 1000)) }
+                bucket.waitingFuture = wait
+                return wait.thenCompose { createRequest(path, bucket) }
+            }
+            bucket.remaining --
+        }
+        
+        return createRequest(path, bucket)
     }
     
     override fun getIfAvailable(): T? {
